370f534e59227c77f106cb3132864a5d5239a5e7
David J. Bradshaw <dave@bradshaw.net>
2026-05-04 13:58:25 +0100

    Formatting

--- src/useKeyboard.js
+++ src/useKeyboard.js
@@ -11,54 +11,57 @@ const isEditableElement = (target) =>
   )
 
 export default function useKeyboard(dispatch) {
-  useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (event.metaKey || event.ctrlKey || event.altKey) return
-      if (isEditableElement(event.target)) return;
+  useEffect(
+    () => {
+      const handleKeyDown = (event) => {
+        if (event.metaKey || event.ctrlKey || event.altKey) return
+        if (isEditableElement(event.target)) return;
 
-      const { key } = event
+        const { key } = event
 
-      if (/^[0-9]$/.test(key)) {
-        dispatch({ type: NUMBER, number: Number(key) })
-      } else switch (key) {
-        case '.':
-          dispatch({ type: DECIMAL })
-          break
+        if (/^[0-9]$/.test(key)) {
+          dispatch({ type: NUMBER, number: Number(key) })
+        } else switch (key) {
+          case '.':
+            dispatch({ type: DECIMAL })
+            break
 
-        case '+':
-        case '-':
-        case '/':
-          dispatch({ type: SYMBOL, symbol: key })
-          break
+          case '+':
+          case '-':
+          case '/':
+            dispatch({ type: SYMBOL, symbol: key })
+            break
 
-        case '*':
-        case 'x':
-        case 'X':
-          dispatch({ type: SYMBOL, symbol: 'X' })
-          break
+          case '*':
+          case 'x':
+          case 'X':
+            dispatch({ type: SYMBOL, symbol: 'X' })
+            break
 
-        case '=':
-        case 'Enter':
-          dispatch({ type: SYMBOL, symbol: '=' })
-          break
+          case '=':
+          case 'Enter':
+            dispatch({ type: SYMBOL, symbol: '=' })
+            break
 
-        case 'Backspace':
-        case 'Delete':
-        case 'Escape':
-        case 'c':
-        case 'C':
-          dispatch({ type: CLEAR })
-          break
+          case 'Backspace':
+          case 'Delete':
+          case 'Escape':
+          case 'c':
+          case 'C':
+            dispatch({ type: CLEAR })
+            break
 
-        default:
-          return
+          default:
+            return
+        }
+
+        event.preventDefault()
       }
 
-      event.preventDefault()
-    }
+      window.addEventListener('keydown', handleKeyDown)
 
-    window.addEventListener('keydown', handleKeyDown)
-
-    return () => window.removeEventListener('keydown', handleKeyDown)
-  }, [dispatch])
+      return () => window.removeEventListener('keydown', handleKeyDown)
+    },
+    [dispatch],
+  )
 }
