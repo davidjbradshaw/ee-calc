1109ad0bfd68e9590857718e593652636916aaab
David J. Bradshaw <dave@bradshaw.net>
2026-05-04 11:40:49 +0100

    replace inline styles with classes

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,22 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
-import { eeBlue } from './consts'
-
-const buttonStyle={
-  backgroundColor: 'white',
-  color: eeBlue,
-  borderRadius: '5px',
-  border: 'solid 2px white',
-  height: '40px',
-  width: '40px',
-  margin: '0 0 5px 5px',
-  textAlign: 'centre',
-  fontSize: '20px'
-}
+import './button.css'
 
 const Button = ({value, onClick}) => (
-  <button type="button" value={value} onClick={() => onClick(value)} style={buttonStyle}>
+  <button type="button" value={value} onClick={() => onClick(value)}>
     {value}
   </button>
 )
--- src/Display.jsx
+++ src/Display.jsx
@@ -1,24 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
-import { eeBlue } from './consts'
-
-const displayStyle={
-  border: `solid 5px ${eeBlue}`,
-  color: 'black',
-  backgroundColor: '#B7C98A',
-  borderRadius: '10px',
-  fontFamily: '"DSEG7 Classic", monospace',
-  fontSize: '22px',
-  fontWeight: '500',
-  letterSpacing: '1px',
-  padding: `5px 10px 5px`,
-  textAlign: 'right',
-  overflow: 'hidden',
-}
+import './display.css'
 
 const Display = ({children}) => (
-  <div style={displayStyle}>{ children }</div>
+  <div className="display">{ children }</div>
 )
 
 Display.prototypes = {
