903cff5fc138e6f9ee910fe848e53a7932407641
David J. Bradshaw <dave@bradshaw.net>
2026-05-04 18:38:02 +0100

    tidy

--- src/consts.js
+++ src/consts.js
@@ -4,8 +4,8 @@ export const SCREEN_MAX = 8
 export const ZERO = '0'
 
 // Action Types
-export const ALL_CLEAR = "AC";
+export const ALL_CLEAR = 'AC'
 export const CLEAR = 'C'
+export const DECIMAL = '.'
 export const NUMBER = Symbol('NUMBER')
 export const SYMBOL = Symbol('SYMBOL')
-export const DECIMAL = '.'
