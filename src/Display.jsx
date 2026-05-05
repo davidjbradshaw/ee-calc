3c8a583b68b3d4ff335111bbd2fb105da5e6b5a0
David J. Bradshaw <dave@bradshaw.net>
2026-05-05 15:04:12 +0100

    tidy display exp

--- src/Display.jsx
+++ src/Display.jsx
@@ -1,6 +1,6 @@
 import React, { memo } from 'react'
 import PropTypes from 'prop-types'
-import { hasDecimal, isScreenFull } from './calcHelpers'
+import { isScreenFull } from './calcHelpers'
 import { SCREEN_MAX } from './consts'
 
 import './Display.css'
@@ -9,23 +9,18 @@ export function formatDisplayValue(value) {
   if (value === 'NaN') return 'Error'
   if (!isScreenFull(value)) return value;
 
-  const maxLength = hasDecimal(value)
-    ? SCREEN_MAX + 1
-    : SCREEN_MAX
-
-
   const numericValue = Number(value)
 
-  if (!Number.isFinite(numericValue)) {
-    return value
-  }
+  if (!Number.isFinite(numericValue))  return value
 
-  let precision = Math.max(0, maxLength - 4)
+  const maxLength = SCREEN_MAX + 2
+  let precision = SCREEN_MAX -4
   let formattedValue = numericValue
     .toExponential(precision)
     .replace('e', ' e')
 
   while (formattedValue.length > maxLength && precision > 0) {
+    console.log('formattedValue', formattedValue, 'precision', precision)
     precision -= 1
     formattedValue = numericValue
       .toExponential(precision)
